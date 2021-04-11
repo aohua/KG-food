a547a70bc8212789ee3ae6a51efefd1097a9bd98
MU AOHUA <aohua.mu@aohua-mac.lan>
2021-04-11 10:54:42 +0800

    add link

---
+++ food-app-web/src/components/Link/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import s from "./index.module.css";
+
+const Link: FC<{ className?: string }> = ({ children, className }) => {
+  return <div className={`${s.link} ${className}`}>{children}</div>;
+};
+
+export default Link;
--- food-app-web/src/pages/Menu/index.tsx
+++ food-app-web/src/pages/Menu/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "components/Header";
+import Links from "components/Links";
+import Link from "components/Link";
 
 function Menu() {
   return (
     <div>
       <Header location="Blk 123 Eunos Ave 1 #12-123" numOfStores={2} est={40} />
+      <Links>
+        <Link>Main</Link>
+        <Link>Putien</Link>
+      </Links>
     </div>
   );
 }
