8430c32ba4c5c47a1235e29b67f01e84582d25c1
MU AOHUA <aohua.mu@aohua-mac.lan>
2021-04-25 23:37:40 +0800

    udpate cache

--- food-app-web-pwa/src/service-worker.ts
+++ food-app-web-pwa/src/service-worker.ts
@@ -13,6 +13,7 @@ import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
+import {CacheFirst} from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -23,6 +24,13 @@ clientsClaim();
 // This variable must be present somewhere in your service worker file,
 // even if you decide not to use precaching. See https://cra.link/PWA
 precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  ({request}) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'image-cache',
+  })
+);
 
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
