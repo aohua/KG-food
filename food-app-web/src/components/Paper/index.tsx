5c20fa4ab8ab9787edd70ed8cb48c557c03f63e7
MU AOHUA <aohua.mu@aohua-mac.local>
2021-04-26 11:27:08 +0800

    remove old app

--- food-app-web/src/components/Paper/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-import s from "./index.module.css";
-
-const Paper: FC<{}> = ({ children }) => {
-  return <div className={s.paper}>{children}</div>;
-};
-
-export default Paper;
